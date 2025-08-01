e14925bf3be568af37e40adad8d90111a5e1573d
PETRO <dim4petro357@yandex.ru>
2025-08-01 15:44:53 +0800

    newchanges1

---
+++ server/src/test/resources/data.sql
@@ -0,0 +1,13 @@
+DELETE FROM users;
+DELETE FROM items;
+DELETE FROM booking;
+DELETE FROM comments;
+DELETE FROM item_request;
+DELETE FROM responses;
+
+INSERT INTO booking_status (id, name) VALUES
+          (1, 'WAITING'),
+          (2, 'APPROVED'),
+          (3, 'REJECTED'),
+          (4, 'CANCELED')
+        ON CONFLICT (id) DO NOTHING;
